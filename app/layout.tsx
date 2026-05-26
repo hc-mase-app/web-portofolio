ed14cad80892555f7e7f75041deac19ad876171a
hc-mase-app <yanfirdaus1984@gmail.com>
2026-05-26 14:21:38 +0700

    logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,9 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
+        url: '/favicon.png'
+      },
+      {
         url: '/icon-light-32x32.png',
         media: '(prefers-color-scheme: light)',
       },
