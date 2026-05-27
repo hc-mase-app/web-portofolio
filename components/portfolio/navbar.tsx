62b806facf7b565c02a89bebfd0059e73c25ec77
hc-mase-app <yanfirdaus1984@gmail.com>
2026-05-27 11:42:46 +0700

    gallery

--- components/portfolio/navbar.tsx
+++ components/portfolio/navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
 
   const navItems = [
     { label: 'Home', href: '/' },
-    { label: 'About', href: '/about' },
+    { label: 'AboutMe', href: '/about' },
     { label: 'Experience', href: '/experience' },
     { label: 'Projects', href: '/projects' },
     { label: 'Evidance', href: '/gallery' },
