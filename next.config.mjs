879de071667035d301c9a64dc550bf925c03006f
hc-mase-app <yanfirdaus1984@gmail.com>
2026-05-28 19:16:53 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,32 +3,8 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-
   images: {
     unoptimized: true,
-  },
-
-  headers: async () => {
-    return [
-      {
-        source: '/cv',
-        headers: [
-          {
-            key: 'Content-Disposition',
-            value: 'attachment; filename="Yan-Firdaus-CV.pdf"',
-          },
-        ],
-      },
-      {
-        source: '/cv.pdf',
-        headers: [
-          {
-            key: 'Content-Disposition',
-            value: 'attachment; filename="Yan-Firdaus-CV.pdf"',
-          },
-        ],
-      },
-    ]
   },
 }
 
