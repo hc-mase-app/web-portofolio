dc074769734d1660a718fcbc69d6ea630f69157b
hc-mase-app <yanfirdaus1984@gmail.com>
2026-05-28 19:01:48 +0700

    Update page.tsx

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -5,7 +5,7 @@ import { useEffect } from 'react'
 export default function CVPage() {
   useEffect(() => {
     const link = document.createElement('a')
-    link.href = '/cv.pdf'
+    link.href = '/cv.pdf?download=true'
     link.download = 'Yan-Firdaus-CV.pdf'
     document.body.appendChild(link)
     link.click()
